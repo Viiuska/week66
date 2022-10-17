1a87d0eb4ef59c63f02dc58c1fa1406aaebdbab8
Viiuska <87257685+Viiuska@users.noreply.github.com>
2022-10-17 16:05:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -71,11 +71,6 @@ const getData = async () => {
 };
 
 const buildChart = async (check, kunta) => {
-  const data = await getData();
-  let years = Object.values(data.dimension.Vuosi.category.label);
-  const alue = Object.values(data.dimension.Alue);
-  const luku = data.value;
-
   if (check === "true") {
     const url =
       "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
@@ -83,29 +78,31 @@ const buildChart = async (check, kunta) => {
       method: "GET"
     });
     const dataCode = await res.json();
+
     const dataC = await getData();
-    let years = Object.values(dataC.dimension.Vuosi.category.label);
+    //let years = Object.values(dataC.dimension.Vuosi.category.label);
 
-    let muniCode = Object.values(dataCode.variables[1]);
-    let infoCode = muniCode[2]; //antaa koodi jono, infoCode[2] paikka
+    let muniCode = Object.values(dataCode.variables[1])[2];
+    //let infoCode = muniCode[2]; //antaa koodi jono, infoCode[2] paikka
 
-    let muniName = Object.values(dataCode.variables[1]);
-    let infoName = muniName[3]; //antaa nimi jonon,infoName[2] paikka
+    let muniName = Object.values(dataCode.variables[1])[3];
+   //console.log(Object.values(dataCode.variables[1])[3])
+    //let infoName = muniName[3]; //antaa nimi jonon,infoName[2] paikka
 
-    let i = 0;
+    let area = "";
     for (let n = 0; n < 310; n++) {
-      if (kunta.toLowerCase() !== infoName[n].toLowerCase()) {
-        i++;
-      } else {
-        const aluecode = infoCode[i];
-        query.query[1].selection.values[0] = aluecode;
-
-        const data = await getData();
-        const luku = data.value;
-        break;
+      if (kunta.toLowerCase() === muniName[n].toLowerCase()) {
+        area = muniCode[n]
+        kunta = muniName[n]
       }
     }
+
+    query.query[1].selection.values[0] = area;
   }
+  const data = await getData();
+  let years = Object.values(data.dimension.Vuosi.category.label);
+  //const alue = Object.values(data.dimension.Alue);
+  const luku = data.value;
 
   let charData = {
     labels: years,
